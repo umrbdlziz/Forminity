b2535702feb3311f6df441b9eff854921e72dd4b
umxziz <umxziz@student.usm.my>
2023-12-07 11:05:45 +0800

    Add Font

---
+++ constants/theme.js
@@ -0,0 +1,21 @@
+const COLORS = {
+  primary: "#FFFBF5",
+  secondary: "#C3ACD0",
+  tertiary: "#8E8FFA",
+
+  text: "#343232",
+  secondtext: "#514D4C",
+
+  background: "#F7EFE5",
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+export { COLORS, SIZES };
